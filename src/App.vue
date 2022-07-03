87341b72cc790e7bda733da2bcb5018b887e7f96
limengting <1443848358@qq.com>
2022-07-03 21:03:28 +0800

    第四步，删除任务

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
-    <TodoHeader @add='addFn'></TodoHeader>
-    <TodoMain :list="list"></TodoMain>
+    <TodoHeader @add="addFn"></TodoHeader>
+    <TodoMain :list="list" @delList="delFn"></TodoMain>
     <TodoFooter></TodoFooter>
   </div>
 </template>
 
 <script>
-import TodoHeader from './components/TodoHeader.vue'
+import TodoHeader from "./components/TodoHeader.vue";
 import TodoMain from "./components/TodoMain";
 import TodoFooter from "./components/TodoFooter";
 export default {
@@ -16,28 +16,33 @@ export default {
       list: [
         { id: 100, name: "吃饭", isDone: true },
         { id: 101, name: "睡觉", isDone: false },
-        { id: 102, name: "打豆豆", isDone: true },
-      ],
+        { id: 102, name: "打豆豆", isDone: true }
+      ]
     };
   },
-components:{
-  TodoHeader,
-  TodoMain,
-  TodoFooter
-},
-methods:{
-  addFn(val) {
-    const id = this.list[this.list.length-1]? this.list[this.list.length-1].id +1:100
-    this.list.push({
-      id,
-      name:val,
-      isDone:false
-    })
+  components: {
+    TodoHeader,
+    TodoMain,
+    TodoFooter
+  },
+  methods: {
+    addFn(val) {
+      const id = this.list[this.list.length - 1]
+        ? this.list[this.list.length - 1].id + 1
+        : 100;
+      this.list.push({
+        id,
+        name: val,
+        isDone: false
+      });
+    },
+    delFn(id) {
+      const index = this.list.findIndex(ele => ele.id === id);
+      this.list.splice(index, 1);
+    }
   }
-}
-}
+};
 </script>
 
 <style>
-
 </style>
