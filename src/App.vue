f64fde20e6cf9d758fa6518f9a1d3f57e57fbf7a
limengting <1443848358@qq.com>
2022-07-03 20:30:40 +0800

    第一步，创建工程和组件

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <TodoHeader></TodoHeader>
+    <TodoMain></TodoMain>
+    <TodoFooter></TodoFooter>
+  </div>
+</template>
+
+<script>
+import TodoHeader from './components/TodoHeader.vue'
+import TodoMain from "./components/TodoMain";
+import TodoFooter from "./components/TodoFooter";
+export default {
+components:{
+  TodoHeader,
+  TodoMain,
+  TodoFooter
+}
+}
+</script>
+
+<style>
+
+</style>
