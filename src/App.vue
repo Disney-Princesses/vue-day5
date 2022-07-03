fbce9993947b0c845a894d3d9458330f435308ca
limengting <1443848358@qq.com>
2022-07-03 20:44:51 +0800

    第二步，铺设待办任务

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <TodoHeader></TodoHeader>
-    <TodoMain></TodoMain>
+    <TodoHeader ></TodoHeader>
+    <TodoMain :list="list"></TodoMain>
     <TodoFooter></TodoFooter>
   </div>
 </template>
@@ -11,6 +11,15 @@ import TodoHeader from './components/TodoHeader.vue'
 import TodoMain from "./components/TodoMain";
 import TodoFooter from "./components/TodoFooter";
 export default {
+  data() {
+    return {
+      list: [
+        { id: 100, name: "吃饭", isDone: true },
+        { id: 101, name: "睡觉", isDone: false },
+        { id: 102, name: "打豆豆", isDone: true },
+      ],
+    };
+  },
 components:{
   TodoHeader,
   TodoMain,
--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -1,19 +1,15 @@
 <template>
   <header class="header">
     <h1>todos</h1>
-    <input id="toggle-all" class="toggle-all" type="checkbox" >
-    <label for="toggle-all"></label> 
+    <input id="toggle-all" class="toggle-all" type="checkbox" />
+    <label for="toggle-all"></label>
     <!-- label 可以关联一个表单标签 -->
-    <input
-      class="new-todo"
-      placeholder="输入任务名称-回车确认"
-      autofocus
-    />
+    <input class="new-todo" placeholder="输入任务名称-回车确认" autofocus />
   </header>
 </template>
 
 <script>
 export default {
- 
-}
+  
+};
 </script>
